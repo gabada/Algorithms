0b282a6ac1e1cf9bd709973cb852791c70970280
Gill Abada <gill@callifi.com>
2019-06-06 19:13:33 -0400

    attempted RPS.

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,8 +2,37 @@
 
 import sys
 
+possible_plays = [['rock'], ['paper'], ['scissors']]
+
+# 2= [['rock', 'rock'], ['rock', 'paper'], ['rock', 'scissors'],
+    # ['paper', 'rock'], ['paper', 'paper'], ['paper', 'scissors'],
+    # ['scissors', 'rock'], ['scissors', 'paper'], ['scissors', 'scissors']]
+
 def rock_paper_scissors(n):
-  pass 
+    if n <= 0:
+        return [[]]
+    if n == 1:
+        return([['rock'], ['paper'], ['scissors']])
+    i = 0
+    j = 0
+    x = []
+    while i < 3:
+        rock = (possible_plays[0] + possible_plays[i] * (n - 1))
+        x.append(rock)
+        paper = (possible_plays[1] + possible_plays[i] * (n - 1))
+        x.append(paper)
+        scissors = (possible_plays[2] + possible_plays[i] * (n - 1))
+        x.append(scissors)
+        i += 1
+
+
+
+        #     plays = ([possible_plays[0] + possible_plays[j] ] * n + [possible_plays[1] + possible_plays[j]] + [possible_plays[2] + possible_plays[j]])
+        #     j+=1
+        #     # print(plays)
+    print(x)
+    # return(x)
+
 
 
 if __name__ == "__main__":
